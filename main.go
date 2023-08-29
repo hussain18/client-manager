1ca9d1580dfde01c192765ff5c47e95a2a85e21c
Talib Hussain Naseri <thussainnt@gmail.com>
2023-08-29 15:39:03 +0530

    add connection to mongodb

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	router := gin.Default()
+	connectDatabase()
+
 	router.GET("/", rootRoute)
 
 	router.Run("localhost:8080")
 }
 
+func connectDatabase() {
+	clientOptions := options.Client().ApplyURI(os.Getenv("DB_URL"))
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatalln("DB_CONN_ERR:", err)
+	}
+
+	log.Println("DB connection successful")
+
+	defer client.Disconnect(context.Background())
+}
+
 func rootRoute(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "API WORKS!"})
 }
-
-// tKE8MMsdRwDRAS9U
-// mongodb+srv://thussainnt:tKE8MMsdRwDRAS9U@cluster0.abiarlh.mongodb.net/?retryWrites=true&w=majority
